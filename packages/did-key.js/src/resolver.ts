88f607e6f58a5ba8f8cd5750e1684d83e9203f8f
Orie Steele <orie@transmute.industries>
2020-06-17 09:03:27 -0500

    cleanup

--- packages/did-key.js/src/resolver.ts
+++ packages/did-key.js/src/resolver.ts
@@ -1,8 +1,3 @@
-import { driver as secp256k1_driver } from '@transmute/did-key-secp256k1';
-import { driver as ed25519_driver } from '@transmute/did-key-ed25519';
-import { driver as x25519_driver } from '@transmute/did-key-x25519';
-import { driver as bls12381_driver } from '@transmute/did-key-bls12381';
-
 export const resolver = {
   resolve: (did: string) => {
     if (did.indexOf('did:key:') !== 0) {
@@ -12,13 +7,13 @@ export const resolver = {
     const encodedType = idchar.substring(0, 4);
     switch (encodedType) {
       case 'z6Mk':
-        return ed25519_driver.get({ did });
+        return require('@transmute/did-key-ed25519').driver.get({ did });
       case 'z6LS':
-        return x25519_driver.get({ did });
+        return require('@transmute/did-key-x25519').driver.get({ did });
       case 'zUC7':
-        return bls12381_driver.get({ did });
+        return require('@transmute/did-key-bls12381').driver.get({ did });
       case 'zQ3s':
-        return secp256k1_driver.get({ did });
+        return require('@transmute/did-key-secp256k1').driver.get({ did });
       default:
         throw new Error('Unknown DID Key type: ' + encodedType);
     }
