13cb3663ccac3ab0a04bedc0e27d4d2ceb1415f2
Orie Steele <orie@transmute.industries>
2020-08-22 11:55:04 -0500

    feat: fix dynamic require

--- packages/did-key.js/src/resolver.ts
+++ packages/did-key.js/src/resolver.ts
@@ -1,23 +1,29 @@
+import { driver as ed25519Driver } from '@transmute/did-key-ed25519';
+import { driver as x25519Driver } from '@transmute/did-key-x25519';
+import { driver as bls12381Driver } from '@transmute/did-key-bls12381';
+import { driver as secp256k1Driver } from '@transmute/did-key-secp256k1';
+import { driver as p384Driver } from '@transmute/did-key-p384';
+
+const prefixToDriverMap: any = {
+  z6Mk: ed25519Driver,
+  z6LS: x25519Driver,
+  zUC7: bls12381Driver,
+  zQ3s: secp256k1Driver,
+  zUew: p384Driver,
+};
+
 export const resolver = {
-  resolve: (did: string) => {
+  resolve: async (did: string) => {
     if (did.indexOf('did:key:') !== 0) {
       throw new Error('did must be of method did:key.');
     }
     const idchar: any = did.split('did:key:').pop();
     const encodedType = idchar.substring(0, 4);
-    switch (encodedType) {
-      case 'z6Mk':
-        return require('@transmute/did-key-ed25519').driver.get({ did });
-      case 'z6LS':
-        return require('@transmute/did-key-x25519').driver.get({ did });
-      case 'zUC7':
-        return require('@transmute/did-key-bls12381').driver.get({ did });
-      case 'zQ3s':
-        return require('@transmute/did-key-secp256k1').driver.get({ did });
-      case 'zUew':
-        return require('@transmute/did-key-p384').driver.get({ did });
-      default:
-        throw new Error('Unknown DID Key type: ' + encodedType);
+    try {
+      const result = await prefixToDriverMap[encodedType].get({ did });
+      return result;
+    } catch (e) {
+      throw new Error('Unknown DID Key type: ' + encodedType);
     }
   },
 };
