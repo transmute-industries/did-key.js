ab199e3f194cf56f12907311d95eec69616d4eb8
gjgd <gjgd@protonmail.com>
2021-04-14 14:18:38 -0500

    feat: use @transmute/did-context for extended did context

--- packages/vc-interop-tests/src/documentLoader.js
+++ packages/vc-interop-tests/src/documentLoader.js
@@ -2,6 +2,7 @@ const {
   documentLoaderFactory,
   contexts,
 } = require("@transmute/jsonld-document-loader");
+const didContext = require('@transmute/did-context');
 
 const { driver } = require("@transmute/did-key-ed25519");
 
@@ -11,31 +12,10 @@ const documentLoader = documentLoaderFactory.pluginFactory
       ...contexts.W3C_Decentralized_Identifiers,
       ...contexts.W3C_Verifiable_Credentials,
       ...contexts.W3ID_Security_Vocabulary,
-    },
-  })
-  .addContext({
-    'https://ns.did.ai/transmute/v1': {
-      '@context': [
-        {
-          '@version': 1.1,
-        },
-        'https://www.w3.org/ns/did/v1',
-        {
-          JsonWebKey2020: 'https://w3id.org/security#JsonWebKey2020',
-          Ed25519VerificationKey2018:
-            'https://w3id.org/security#Ed25519VerificationKey2018',
-          X25519KeyAgreementKey2019:
-            'https://w3id.org/security#X25519KeyAgreementKey2019',
-
-          publicKeyJwk: {
-            '@id': 'https://w3id.org/security#publicKeyJwk',
-            '@type': '@json',
-          },
-          publicKeyBase58: {
-            '@id': 'https://w3id.org/security#publicKeyBase58',
-          },
-        },
-      ],
+      [didContext.constants
+        .DID_CONTEXT_TRANSMUTE_V1_URL]: didContext.contexts.get(
+        didContext.constants.DID_CONTEXT_TRANSMUTE_V1_URL
+      ),
     },
   })
   .addResolver({
